7dd2a8eaa370782402f793a8d2142a17cd5d2968
Philipp <press.fm@mail.ru>
2023-02-03 23:19:25 +0300

    Написал код результата

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -5,11 +5,13 @@
     Console.Write ($"{array[i]} ");
 }
 
-
-
-
-
-
+void PrintArray2 (string [] array)
+{
+    Console.WriteLine ($"Начальный массив: {array}");
+    for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= 3)
+    Console.Write ($"{array[i]} ");
+}
 
 Console.Clear();
 string [] array = new string [5] {"123", "test", "word", "or", "mail"};
