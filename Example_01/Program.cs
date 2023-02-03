4831a06ced9974ca2e2c4be61818f1267a4f2002
Philipp <press.fm@mail.ru>
2023-02-03 23:18:24 +0300

    Написал 1 часть

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+
+
+
+
+
+
+Console.Clear();
+string [] array = new string [5] {"123", "test", "word", "or", "mail"};
+PrintArray1 (array);
+Console.WriteLine();
+Console.WriteLine ("Результат");
+PrintArray2(array);
